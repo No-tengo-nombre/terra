160eea247dd87e344ec63c897b557e049785df34
Cristóbal Allendes <callendes.molina@gmail.com>
2024-02-24 15:29:18 -0300

    Added macros for internal Vulkan calls

--- include/terrau/macros.h
+++ include/terrau/macros.h
@@ -2,7 +2,13 @@
 #include <terra/status.h>
 
 #define TERRA_CALL(x, msg, ...)                                                \
-  if (x != TERRA_STATUS_SUCCESS) {                                      \
+  if (x != TERRA_STATUS_SUCCESS) {                                             \
+    log_error(msg, #__VA_ARGS__);                                              \
+    return TERRA_STATUS_FAILURE;                                               \
+  }
+
+#define TERRA_VK_CALL(x, msg, ...)                                             \
+  if (x != VK_SUCCESS) {                                                       \
     log_error(msg, #__VA_ARGS__);                                              \
     return TERRA_STATUS_FAILURE;                                               \
   }
--- src/terra_utils/macros.h
+++ src/terra_utils/macros.h
@@ -2,7 +2,13 @@
 #include <terra/status.h>
 
 #define TERRA_CALL_I(x, msg, ...)                                              \
-  if (x != TERRA_STATUS_SUCCESS) {                                      \
+  if (x != TERRA_STATUS_SUCCESS) {                                             \
+    logi_error(msg, #__VA_ARGS__);                                             \
+    return TERRA_STATUS_FAILURE;                                               \
+  }
+
+#define TERRA_VK_CALL_I(x, msg, ...)                                           \
+  if (x != VK_SUCCESS) {                                                       \
     logi_error(msg, #__VA_ARGS__);                                             \
     return TERRA_STATUS_FAILURE;                                               \
   }

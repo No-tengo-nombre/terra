42fe558bbd238b0be53eecf900d279d1c68b90f1
Cristóbal Allendes <callendes.molina@gmail.com>
2024-02-25 19:00:20 -0300

    Started working on an example that displays a triangle on screen

---
+++ examples/triangle/main.c
@@ -0,0 +1,38 @@
+#include <terra/app.h>
+#include <terra/setup.h>
+#include <terra/vk/swapchain.h>
+#include <terrau/log.h>
+#include <terrau/macros.h>
+
+terra_status_t start(terra_app_t *app) {
+  TERRA_CALL(terra_init(app, NULL), "Failed initializing app");
+
+  return TERRA_STATUS_SUCCESS;
+}
+
+terra_status_t loop(terra_app_t *app) {
+  if (terra_app_should_close(app)) {
+    log_info("Terminating program loop");
+    return TERRA_STATUS_EXIT;
+  }
+
+  glfwPollEvents();
+
+  return TERRA_STATUS_SUCCESS;
+}
+
+terra_status_t cleanup(terra_app_t *app) {
+  TERRA_CALL(terra_app_cleanup(app), "Failed cleanup");
+  return TERRA_STATUS_SUCCESS;
+}
+
+int main(void) {
+  terra_app_metadata_t meta = terra_app_metadata_default();
+  meta.app_name = "Terra (example) - Triangle";
+
+  terra_app_config_t conf = terra_app_config_default();
+
+  terra_app_t app;
+  terra_app_new(&start, &loop, &cleanup, &meta, &conf, &app);
+  return terra_app_run(&app);
+}

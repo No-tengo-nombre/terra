f6c318de55d5ba640eb6c0b075ff03d8f12a971b
Cristóbal Allendes <callendes.molina@gmail.com>
2024-03-03 00:38:24 -0300

    Added functions to await the sync objects in an app

--- src/terra_core/vk/sync.c
+++ src/terra_core/vk/sync.c
@@ -13,6 +13,8 @@ terra_status_t terra_vk_create_sync_objects(terra_app_t *app) {
 
   VkFenceCreateInfo in_flight_info = {VK_FALSE};
   in_flight_info.sType             = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+  in_flight_info.flags             = VK_FENCE_CREATE_SIGNALED_BIT;
+  // The flag ensures the first draw call does not fail
 
   TERRA_VK_CALL_I(
       vkCreateSemaphore(
@@ -34,6 +36,25 @@ terra_status_t terra_vk_create_sync_objects(terra_app_t *app) {
           app->vk_ldevice, &in_flight_info, NULL, &app->vk_in_flight_F
       ),
       "Failed creating 'vk_in_flight_F' fence"
+  );
+
+  return TERRA_STATUS_SUCCESS;
+}
+
+terra_status_t terra_vk_await_sync_objects(terra_app_t *app) {
+  TERRA_VK_CALL_I(
+      vkWaitForFences(
+          app->vk_ldevice,
+          1,
+          &app->vk_in_flight_F,
+          VK_TRUE,
+          app->conf->in_flight_fence_timeout
+      ),
+      "Failed waiting for fence (check timeout)"
+  );
+  TERRA_VK_CALL_I(
+      vkResetFences(app->vk_ldevice, 1, &app->vk_in_flight_F),
+      "Failed resetting fence"
   );
 
   return TERRA_STATUS_SUCCESS;

fae1c1ccf05dc65ccc95b04440658ea8a7b11e9b
Cristóbal Allendes <callendes.molina@gmail.com>
2024-03-07 20:29:10 -0300

    Changed the way the memory bindings are handled

--- include/terrau/mem.h
+++ include/terrau/mem.h
@@ -7,10 +7,21 @@
 extern "C" {
 #endif
 
+#ifdef NDEBUG
+
+  #define terrau_malloc(app, x)     malloc(x)
+  #define terrau_realloc(app, x, y) realloc(x, y)
+  #define terrau_calloc(app, x, y)  calloc(x, y)
+  #define terrau_free(app, x)       free(x)
+
+#else
+
 void *terrau_malloc(terra_app_t *app, size_t size);
 void *terrau_realloc(terra_app_t *app, void *ptr, size_t new_size);
 void *terrau_calloc(terra_app_t *app, size_t num, size_t size);
 void terrau_free(terra_app_t *app, void *ptr);
+
+#endif
 
 #ifdef __cplusplus
 }
--- src/terra_utils/mem.c
+++ src/terra_utils/mem.c
@@ -4,52 +4,41 @@
 #include <terra/app.h>
 #include <terrau/mem.h>
 
-inline void *terrau_malloc(terra_app_t *app, size_t size) {
 #ifndef NDEBUG
+
+inline void *terrau_malloc(terra_app_t *app, size_t size) {
   logi_debug("Allocating %i B in heap with malloc", size);
   void *ret = malloc(size);
   if (ret != NULL) {
     app->_idebug_malloced_total++;
   }
   return ret;
-#else
-  return malloc(size);
-#endif
 }
 
 inline void *terrau_realloc(terra_app_t *app, void *ptr, size_t new_size) {
-#ifndef NDEBUG
   logi_debug("Allocating %i B in heap with 'realloc'", new_size);
   void *ret = realloc(ptr, new_size);
   if (ret != NULL) {
     app->_idebug_malloced_total++;
   }
   return ret;
-#else
-  return realloc(ptr, new_size);
-#endif
 }
 
 inline void *terrau_calloc(terra_app_t *app, size_t num, size_t size) {
-#ifndef NDEBUG
   logi_debug("Allocating %ix(%i B) in heap with 'calloc'", num, size);
   void *ret = calloc(num, size);
   if (ret != NULL) {
     app->_idebug_malloced_total++;
   }
   return ret;
-#else
-  return calloc(num, size);
-#endif
 }
 
 inline void terrau_free(terra_app_t *app, void *ptr) {
-#ifndef NDEBUG
-  // logi_debug("Freeing memory in heap at address %#18x", ptr);
   logi_debug("Freeing memory in heap at address %#10x", ptr);
   if (ptr != NULL) {
     app->_idebug_malloced_total--;
   }
-#endif
   free(ptr);
 }
+
+#endif

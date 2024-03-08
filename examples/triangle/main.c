44f1244d745b8b2134d92f090baacbc4322f0a92
Cristóbal Allendes <callendes.molina@gmail.com>
2024-03-08 16:36:34 -0300

    Made the triangle example use meshes

--- examples/triangle/main.c
+++ examples/triangle/main.c
@@ -2,11 +2,19 @@
 #include <terrau/log.h>
 #include <terrau/macros.h>
 
+terra_vertex3_t triangle_vertices[] = {
+    {{0.0f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
+    { {0.5f, 0.5f, 0.0f}, {0.0f, 1.0f, 0.0f, 1.0f}},
+    {{-0.5f, 0.5f, 0.0f},  {0.0f, 0.0f, 1.0, 1.0f}}
+};
+terra_vector_t triangle_v;
+terra_mesh_t triangle_m;
+
 terra_status_t start(terra_app_t *app) {
   TERRA_CALL(terra_init(app, NULL), "Failed initializing app");
 
   // TODO: Move the rest of the initializations into the terra_init function
-  log_info("Initialized app, creating pipeline");
+  log_info("Creating pipeline");
   TERRA_CALL(
       terra_vk_pipeline_from_filenames(
           app,
@@ -20,6 +28,23 @@ terra_status_t start(terra_app_t *app) {
   TERRA_CALL(terra_vk_command_pool_new(app), "Failed creating command pool");
   TERRA_CALL(terra_vk_create_sync_objects(app), "Failed creating sync objects");
 
+  log_info("Initialized application, reading shapes");
+  log_debug("Loading triangle");
+  TERRA_CALL(
+      terra_vector_from_array(
+          app,
+          triangle_vertices,
+          sizeof(triangle_vertices),
+          sizeof(triangle_vertices[0]),
+          &triangle_v
+      ),
+      "Failed reading triangle"
+  );
+  TERRA_CALL(
+      terra_mesh_new(app, &triangle_v, &triangle_m), "Failed to create mesh"
+  );
+  TERRA_CALL(terra_mesh_push(app, &triangle_m), "Failed to push triangle");
+
   return TERRA_STATUS_SUCCESS;
 }
 
@@ -28,18 +53,23 @@ terra_status_t loop(terra_app_t *app) {
     log_info("Terminating program loop");
     return TERRA_STATUS_EXIT;
   }
-  TERRA_CALL(terra_app_draw(app), "Failed high-level draw call");
-
   glfwPollEvents();
+
+  TERRA_CALL(terra_mesh_update(app, &triangle_m), "Failed updating mesh");
+  TERRA_CALL(terra_app_draw(app), "Failed high-level draw call");
 
   return TERRA_STATUS_SUCCESS;
 }
 
 terra_status_t cleanup(terra_app_t *app) {
-  // This function could be removed and the associated argument in the
-  // `terra_app_new` call be passed as NULL, but it is left to show how
-  // it can be specified
-  log_info("Nothing to clean up!");
+  log_info("Cleaning up shapes");
+  TERRA_CALL(
+      terra_vector_cleanup(app, &triangle_v),
+      "Failed to clean up triangle vertices"
+  );
+  TERRA_CALL(
+      terra_mesh_cleanup(app, &triangle_m), "Failed to clean up triangle mesh"
+  );
   return TERRA_STATUS_SUCCESS;
 }
 

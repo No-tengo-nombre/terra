38505cf00a3a69f04ebbe6fa72762e5b7cd94ebe
Cristóbal Allendes <callendes.molina@gmail.com>
2024-03-09 19:31:48 -0300

    Added name mappings for the VkResult struct

--- include/terra/vk/name_mappings.h
+++ include/terra/vk/name_mappings.h
@@ -9,6 +9,7 @@ extern "C" {
 const char *terra_vk_format_name(VkFormat format);
 const char *terra_vk_colorspace_name(VkColorSpaceKHR color_space);
 const char *terra_vk_present_mode_name(VkPresentModeKHR present_mode);
+const char *terra_vk_result_name(VkResult res);
 
 #ifdef __cplusplus
 }

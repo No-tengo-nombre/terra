160bb3f5d525f2961165424a89062e3af67094b1
Cristóbal Allendes <callendes.molina@gmail.com>
2024-03-09 19:48:35 -0300

    Made the memory allocation functions be macros that pass filename and line information

--- include/terrau/mem.h
+++ include/terrau/mem.h
@@ -16,10 +16,25 @@ extern "C" {
 
 #else
 
-void *terrau_malloc(terra_app_t *app, size_t size);
-void *terrau_realloc(terra_app_t *app, void *ptr, size_t new_size);
-void *terrau_calloc(terra_app_t *app, size_t num, size_t size);
-void terrau_free(terra_app_t *app, void *ptr);
+  #define __FILENAME__ (__FILE__ + SOURCE_PATH_SIZE)
+
+void *_terrau_malloc(
+    terra_app_t *app, size_t size, const char *filename, int line
+);
+void *_terrau_realloc(
+    terra_app_t *app, void *ptr, size_t new_size, const char *filename, int line
+);
+void *_terrau_calloc(
+    terra_app_t *app, size_t num, size_t size, const char *filename, int line
+);
+void _terrau_free(terra_app_t *app, void *ptr, const char *filename, int line);
+
+  #define terrau_malloc(app, x) _terrau_malloc(app, x, __FILENAME__, __LINE__)
+  #define terrau_realloc(app, x, y)                                            \
+    _terrau_realloc(app, x, y, __FILENAME__, __LINE__)
+  #define terrau_calloc(app, x, y)                                             \
+    _terrau_calloc(app, x, y, __FILENAME__, __LINE__)
+  #define terrau_free(app, x) _terrau_free(app, x, __FILENAME__, __LINE__)
 
 #endif
 
--- src/terra_utils/mem.c
+++ src/terra_utils/mem.c
@@ -6,8 +6,12 @@
 
 #ifndef NDEBUG
 
-extern inline void *terrau_malloc(terra_app_t *app, size_t size) {
-  logi_debug("Allocating %i B in heap with malloc", size);
+extern inline void *_terrau_malloc(
+    terra_app_t *app, size_t size, const char *filename, int line
+) {
+  logi_debug(
+      "Allocating %i B in heap with malloc @ %s:%d", size, filename, line
+  );
   void *ret = malloc(size);
   if (ret != NULL) {
     app->_idebug_malloced_total++;
@@ -15,10 +19,12 @@ extern inline void *terrau_malloc(terra_app_t *app, size_t size) {
   return ret;
 }
 
-extern inline void *terrau_realloc(
-    terra_app_t *app, void *ptr, size_t new_size
+extern inline void *_terrau_realloc(
+    terra_app_t *app, void *ptr, size_t new_size, const char *filename, int line
 ) {
-  logi_debug("Allocating %i B in heap with 'realloc'", new_size);
+  logi_debug(
+      "Allocating %i B in heap with 'realloc' @ %s:%d", new_size, filename, line
+  );
   void *ret = realloc(ptr, new_size);
   if (ret != NULL) {
     app->_idebug_malloced_total++;
@@ -26,8 +32,16 @@ extern inline void *terrau_realloc(
   return ret;
 }
 
-extern inline void *terrau_calloc(terra_app_t *app, size_t num, size_t size) {
-  logi_debug("Allocating %ix(%i B) in heap with 'calloc'", num, size);
+extern inline void *_terrau_calloc(
+    terra_app_t *app, size_t num, size_t size, const char *filename, int line
+) {
+  logi_debug(
+      "Allocating %ix(%i B) in heap with 'calloc' @ %s:%d",
+      num,
+      size,
+      filename,
+      line
+  );
   void *ret = calloc(num, size);
   if (ret != NULL) {
     app->_idebug_malloced_total++;
@@ -35,8 +49,12 @@ extern inline void *terrau_calloc(terra_app_t *app, size_t num, size_t size) {
   return ret;
 }
 
-extern inline void terrau_free(terra_app_t *app, void *ptr) {
-  logi_debug("Freeing memory in heap at address %#10x", ptr);
+extern inline void _terrau_free(
+    terra_app_t *app, void *ptr, const char *filename, int line
+) {
+  logi_debug(
+      "Freeing memory in heap at address %#10x @ %s:%d", ptr, filename, line
+  );
   if (ptr != NULL) {
     app->_idebug_malloced_total--;
   }

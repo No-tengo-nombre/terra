db80de4e82fcf0f951447eb7b3ad2cdc47314fed
Cristóbal Allendes <callendes.molina@gmail.com>
2024-03-09 19:32:05 -0300

    Format

--- src/terra_utils/macros.h
+++ src/terra_utils/macros.h
@@ -17,7 +17,7 @@
     if (err_ != VK_SUCCESS) {                                                  \
       const char *err_str = terra_vk_result_name(err_);                        \
       logi_error(msg, #__VA_ARGS__);                                           \
-      logi_error("Vulkan error [%d]'%s'", err_, err_str);                     \
+      logi_error("Vulkan error [%d]'%s'", err_, err_str);                      \
       return TERRA_STATUS_FAILURE;                                             \
     }                                                                          \
   }

2a73189273e1251f52cf253466f3c56dfb9f37a5
Cristóbal Allendes <callendes.molina@gmail.com>
2024-03-15 16:36:28 -0300

    Made the traces be white

--- src/terra_utils/vendor/log.c
+++ src/terra_utils/vendor/log.c
@@ -44,7 +44,7 @@ static const char *level_strings[] = {
 
 #ifdef LOG_USE_COLOR
 static const char *level_colors[] = {
-    "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
+    "\x1b[37m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
 };
 static const char *level_bg_colors[] = {
     "\x1b[0m", "\x1b[0m", "\x1b[0m", "\x1b[0m", "\x1b[37;41m", "\x1b[37;45m"

46f682a5fa079ee21fba80aa4079113ff9a2e04e
Cristóbal Allendes <callendes.molina@gmail.com>
2024-08-11 19:43:26 -0400

    Set default size of descriptors' data size to vertex3

--- include/terrau/formats/descriptors.h
+++ include/terrau/formats/descriptors.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <terra/data/vector.h>
+#include <terra/data/vertex3.h>
 
-#define TERRA_DEFAULT_DATA_SIZE sizeof(double)
+#define TERRA_DEFAULT_DATA_SIZE sizeof(terra_vertex3_t)
 
 typedef struct terrau_mesh_descriptor {
   terra_vector_t *verts;

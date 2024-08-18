99b62863f2e0a27c35655dd62e45875ac2efe084
Cristóbal Allendes <callendes.molina@gmail.com>
2024-08-18 00:53:33 -0400

    Made meshes own the vectors of vertices and indices

--- include/terra/mesh.h
+++ include/terra/mesh.h
@@ -11,8 +11,8 @@ extern "C" {
 #endif
 
 typedef struct terra_mesh {
-  terra_vector_t *verts;
-  terra_vector_t *idx;
+  terra_vector_t verts;
+  terra_vector_t idx;
   terra_buffer_t vert_buf;
   terra_buffer_t vert_sbuf;
   terra_buffer_t idx_buf;
--- include/terrau/formats/descriptors.h
+++ include/terrau/formats/descriptors.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <stdint.h>
 #include <terra/data/vector.h>
 #include <terra/data/vertex3.h>
 
-#define TERRA_DEFAULT_DATA_SIZE sizeof(terra_vertex3_t)
+#define TERRA_DEFAULT_VERTEX3_DSIZE sizeof(terra_vertex3_t)
+#define TERRA_DEFAULT_FACE3_DSIZE   sizeof(uint32_t)
 
 typedef struct terrau_mesh_descriptor {
-  terra_vector_t *verts;
-  terra_vector_t *idx;
+  terra_vector_t verts;
+  terra_vector_t idx;
 } terrau_mesh_descriptor_t;
